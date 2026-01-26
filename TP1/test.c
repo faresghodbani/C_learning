90ce7db0fc887a2019f2f46abbd2579ab9bfdc20
Fares <faresslaw@gmail.com>
2026-01-26 14:21:59 +0100

    Add files via upload

---
+++ TP1/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    if ('A' == "A") {
+        printf("Tous le monde à 20/20 dans l’UE de C\n");
+    } else {
+        printf("Désolé, mais ça ne marche pas\n");
+    }
+    return 0;
+}
